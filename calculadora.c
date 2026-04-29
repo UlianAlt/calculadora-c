1e936247458fff67063ea8a39e5ce7a3482fe953
UlianAlt <zpedro237732@gmail.com>
2026-04-29 20:32:32 -0300

    Add files via upload

---
+++ calculadora.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "function_multiplicar.h"
+
+int main(){
+    int input = 0;
+    int num1 = 0;
+    int num2 = 0;
+
+    printf("Calculadora\n\n");
+    printf("1 - Soma\n");
+    printf("2 - Subtracao\n");
+    printf("3 - Divisao\n");
+    printf("4 - Multiplicacao\n\n");
+    printf("--> ");
+    scanf("%d", &input);
+    
+    switch(input){
+        case 4:
+            printf("\n\nDigite o primeiro numero: ");
+            scanf("%d", &num1);
+            printf("Digite o segundo numero: ");
+            scanf("%d", &num2);
+            printf("O produto e: %d", multiplicar(num1, num2));
+            break;
+        
+        default:
+            printf("opcao indisponivel");
+            break;
+        
+    }
+
+    return 0;
+}
